6ecd506519b8020a15662ebf5685a9466a1b6567
kellyluo <kellyluo25@gmail.com>
2022-03-19 21:03:02 -0700

    original

--- js/longform_controller.js
+++ js/longform_controller.js
@@ -1,14 +1,31 @@
 // entries is the json service
 
+//// new
+//app_longform.controller('longform_controller', ['$scope', 'entries',
+//    function($scope, entries) {
+//      entries.then(function(data) {
+//          
+//        // set 'entries' as data from controller 
+//        $scope.entries = data;
+//        
+//       });  
+//}]);
+//
+
+
 // new
 app_longform.controller('longform_controller', ['$scope', 'entries',
     function($scope, entries) {
       entries.then(function(data) {
           
+        console.log(data)
+
         // set 'entries' as data from controller 
         $scope.entries = data.records;
-    });  
+        
+       });  
 }]);
+
 
 //oldreads
 app_longform.controller('oldreads_controller', ['$scope', 'oldentries',
--- js/longform_entries.js
+++ js/longform_entries.js
@@ -1,46 +1,68 @@
 // service to fetch 'entries' from json data (via airtable API)
+//
+//var Airtable = require('airtable');
+//
+//
+//// new 
+//var base = new Airtable({apiKey: 'keyjxzR8ckt48ealv'}).base('app5lKJ3fALLBkCtj');
+//
+//
+//
+//async function main_reads() {
+//     var table_reads = await base('reads').select({ 
+//          view: "all",
+//          sort: [{field: "read_date", direction: "desc"}]
+//      }).all();
+//    
+//    console.log(table_reads)
+//    
+//    app_longform.factory('entries', function() {
+//        return table_reads;
+//    });
+//};
+//
+//
+//main_reads();
 
-var Airtable = require('airtable');
+
+
+
+//table_reads('reads').select({
+//    view: "all",
+//    sort: [{field: "read_date", direction: "desc"}]
+//}).eachPage(function page(records, fetchNextPage) {
+//    // To fetch the next page of records, call `fetchNextPage`.
+//    // If there are more records, `page` will get called again.
+//    // If there are no more records, `done` will get called.
+//    fetchNextPage();
+//
+//}, function done(err) {
+//    if (err) { console.error(err); return; }
+//});
+
+// resolve promise into entries
+
+
+//Promise.resolve(table_reads).then(data=>{
+//    entriesdata = data;
+//    console.log(entriesdata);
+//});
 
 
 // new 
+app_longform.factory('entries', ['$http', function($http) {
+  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/reads?api_key=keyjxzR8ckt48ealv&sort%5B0%5D%5Bfield%5D=read_date&sort%5B0%5D%5Bdirection%5D=desc')
+        .then(function(response) {
+          // return data of the GET response
+          return response.data;
+        })
+}]);
 
-var table_reads = new Airtable({apiKey: 'keyjxzR8ckt48ealv'}).base('app5lKJ3fALLBkCtj');
-
-table_reads('reads').select({
-    view: "all",
-    field: "read_date", 
-    direction: "desc"
-}).eachPage(function page(records, fetchNextPage) {
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
-
-var entries = table_reads
-
-console.log(entries)
-
-
-//
-//// new 
-//app_longform.factory('entries', ['$http', function($http) {
-//  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/reads?api_key=keyjxzR8ckt48ealv&sort%5B0%5D%5Bfield%5D=read_date&sort%5B0%5D%5Bdirection%5D=desc')
-//        .then(function(response) {
-//          // return data of the GET response
-//          return response.data;
-//        })
-//}]);
-//
-//// oldreads
-//app_longform.factory('oldentries', ['$http', function($http) {
-//  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/oldreads?api_key=keyjxzR8ckt48ealv')
-//        .then(function(response) {
-//          // return data of the GET response
-//          return response.data;
-//        })
-//}]);
+// oldreads
+app_longform.factory('oldentries', ['$http', function($http) {
+  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/oldreads?api_key=keyjxzR8ckt48ealv')
+        .then(function(response) {
+          // return data of the GET response
+          return response.data;
+        })
+}]);
