a89c042a91ff47116cc1915bd66a1a4b091573ad
kellyluo <kellyluo25@gmail.com>
2022-03-19 19:13:39 -0700

    onceagain

--- js/longform_entries.js
+++ js/longform_entries.js
@@ -1,19 +1,51 @@
 // service to fetch 'entries' from json data (via airtable API)
 
-// new 
-app_longform.factory('entries', ['$http', function($http) {
-  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/reads?api_key=keyjxzR8ckt48ealv&sort%5B0%5D%5Bfield%5D=read_date&sort%5B0%5D%5Bdirection%5D=desc')
-        .then(function(response) {
-          // return data of the GET response
-          return response.data;
-        })
-}]);
+var Airtable = require('airtable');
 
-// oldreads
-app_longform.factory('oldentries', ['$http', function($http) {
-  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/oldreads?api_key=keyjxzR8ckt48ealv')
-        .then(function(response) {
-          // return data of the GET response
-          return response.data;
-        })
-}]);
+
+// new 
+
+var table_reads = new Airtable({apiKey: 'keyjxzR8ckt48ealv'}).base('app5lKJ3fALLBkCtj');
+
+table_reads('reads').select({
+    view: "all",
+    field: "read_date", 
+    direction: "desc"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('title'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+app_longform.factory('entries', return table_reads)
+console.log(table_reads)
+
+
+//
+//// new 
+//app_longform.factory('entries', ['$http', function($http) {
+//  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/reads?api_key=keyjxzR8ckt48ealv&sort%5B0%5D%5Bfield%5D=read_date&sort%5B0%5D%5Bdirection%5D=desc')
+//        .then(function(response) {
+//          // return data of the GET response
+//          return response.data;
+//        })
+//}]);
+//
+//// oldreads
+//app_longform.factory('oldentries', ['$http', function($http) {
+//  return $http.get('https://api.airtable.com/v0/app5lKJ3fALLBkCtj/oldreads?api_key=keyjxzR8ckt48ealv')
+//        .then(function(response) {
+//          // return data of the GET response
+//          return response.data;
+//        })
+//}]);
